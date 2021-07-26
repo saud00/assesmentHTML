c046e3a5dae5b06d16235252863f2dfb752c742e
Saud <gomail2017@gmail.com>
2021-07-26 22:22:03 +0500

    changing HTML through JS

--- App.js
+++ App.js
@@ -1,1 +1,34 @@
-alert("hello");
+const makeElementParent = document.createElement("div");
+makeElementParent.className = "div1";
+makeElementParent.id = "2";
+
+const makeElement = document.createElement("span");
+makeElement.className = "span1";
+makeElement.id = "1";
+
+makeElement.appendChild(
+  document.createTextNode("I am created through pure JS")
+);
+
+//appendChild is used to insert anything
+makeElementParent.appendChild(makeElement);
+
+document.body.appendChild(makeElementParent);
+
+console.log(makeElement.parentElement);
+
+// If you want to place Element in any perticular div or ul or any Element than first select that than add it to it.
+
+// document.querySelector('#1').appendChild(makeElement)
+
+// Replace item
+
+const newSpan = document.createElement("span");
+const newText = newSpan.appendChild(
+  document.createTextNode("I am overWriting previous line")
+);
+
+const parentDiv = document.querySelector(".div1");
+const oldText = document.querySelector(".span1");
+
+parentDiv.replaceChild(newText, oldText);
