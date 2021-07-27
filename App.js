5cf627711fcf951d884005d01efb87534922014a
Saud <gomail2017@gmail.com>
2021-07-27 23:48:50 +0500

    EventListners, keyboard listners, Event Delgations

--- App.js
+++ App.js
@@ -32,3 +32,42 @@ const parentDiv = document.querySelector(".div1");
 const oldText = document.querySelector(".span1");
 
 parentDiv.replaceChild(newText, oldText);
+
+// set Attribute
+
+const btn = document.createElement("button");
+btn.id = "3";
+console.log(btn);
+btn.appendChild(document.createTextNode("button"));
+document.getElementById("2").appendChild(btn);
+
+//set Attribute
+
+btn.setAttribute("type", "submit");
+const check = btn.getAttribute("type");
+console.log(check);
+
+// Add Event Listner like onclick, onSubmit etc
+
+document.getElementById("3").addEventListener("click", onClick);
+
+function onClick(e) {
+  //e.toElement.autofocus = true;
+  console.log(e);
+}
+
+//////
+document.addEventListener("mousemove", function (e) {
+  document.body.style.backgroundColor = `rgb(${e.offsetX} , ${e.offsetY} , 40 )`;
+});
+
+// Event Delgation
+// We donot select item directly by Id or class, instead  parent Element
+// than apply condition on it
+
+document.body.addEventListener("click", function (e) {
+  if (e.target.classList.contains("email")) {
+    console.log(e.className);
+    e.target.parentElement.parentElement.remove();
+  }
+});
